f985ae0dea394fd05d2f388a2b2dc608b510b2d3
Šimon Kocúrek <simon.kocurek@gmail.com>
2015-12-20 13:49:21 +0100

    20. 12. 2015

--- Day20_Infinite_Elves_and_Infinite_Houses.java
+++ Day20_Infinite_Elves_and_Infinite_Houses.java
@@ -5,29 +5,34 @@ import java.util.Scanner;
 public class Day20_Infinite_Elves_and_Infinite_Houses {
 	public static void findHouse() {
 		Scanner input = new Scanner(System.in);
-		int searched =  input.nextInt() / 10;
+		int searched =  input.nextInt();
+		int range = 1;
 
 		findIt:
 		while (true) {
-			int range = 1000;
 			int house[] = new int[range];
+
 			fillHouses(range, house);
 
 			for (int i = 0; i < range; i ++) {
-				if (house[i] == searched) {
+				if (house[i] >= searched) {
 					System.out.println( i);
 					break findIt;
 				}
 			}
-			System.out.println("fail");
+			range *= 2;
 		}
 	}
 
 	private static void fillHouses(int range, int[] presents) {
 		for (int i = 0; i < range; i ++) {
 			int difference = i + 1;
-			for (int n = difference; n < range; n += difference) {
-				presents[difference - 1] = n;
+			for (int n = difference, visits = 0; n < range; n += difference) {
+				visits ++;
+				presents[n] += difference * 11;
+
+				if (visits == 50)
+					break;
 			}
 		}
 	}
